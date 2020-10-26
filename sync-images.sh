0ce27f310c33b82a6c999b987c5689413c000983
guoruibiao <1064319632@qq.com>
2020-10-26 15:18:57 +0800

    refresh

--- sync-images.sh
+++ sync-images.sh
@@ -3,3 +3,9 @@ if [ ! -d "../public/forarticle"  ];then mkdir ../public/forarticle;fi
 
 cp ./_data/forarticle/* ../public/forarticle
 cp ./_data/*.*          ../public/photo
+
+git add .
+timestr=`date`
+git commit -m "更新于 ${timestr}"
+git push origin master
+
